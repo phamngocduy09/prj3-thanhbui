9433a2b9dced7232afa40fc2ea02402c7cd1b94b
phamngocduy09 <duyphamngoc09@gmail.com>
2024-08-07 09:39:38 +0700

    first commit

--- set_env.sh
+++ set_env.sh
@@ -1,9 +1,9 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
+export POSTGRES_USERNAME=duytest
+export POSTGRES_PASSWORD=duytest
+export POSTGRES_HOST=localhost
+export POSTGRES_DB=duytest
 export AWS_BUCKET=arn:aws:s3:::mybucket1200798
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
